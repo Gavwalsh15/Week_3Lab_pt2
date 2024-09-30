cd09a1cbc9754c67f25d46991d4e38b355300d8f
Gavin <G00402296@atu.ie>
2024-09-30 16:56:31 +0100

    Init

---
+++ src/main/java/ie/atu/week_3b/Week3BApplication.java
@@ -0,0 +1,14 @@
+package ie.atu.week_3b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class Week3BApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Week3BApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/week_3b/Week3BApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week_3b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3BApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
